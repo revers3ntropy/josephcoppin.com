826ddffcdab93d397c890425a0b71c9820226051
revers3ntropy <josephcoppin@gmail.com>
2023-04-11 20:33:30 +0100

    Start CV in Svelte

---
+++ cv/src/App.svelte
@@ -0,0 +1,246 @@
+<main>
+    <p>
+        <a href="https://linkedin.com/in/joseph-coppin-9935b71ba">LinkedIn</a>
+        <a href="http://github.com/revers3ntropy">GitHub</a>
+        <a href="https://www.npmjs.com/~revers3ntropy">NPM</a>
+    </p>
+
+    <h1>
+        Hi, I'm Joseph Coppin
+    </h1>
+    <section>
+        <h2>Skills</h2>
+
+        Languages, roughly ordered by proficiency
+
+        <ol>
+            <li>TypeScript / JS</li>
+            <li>Python</li>
+            <li>Rust</li>
+            <li>HTML & CSS</li>
+            <li>C++</li>
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>Go</li>
+            <li>C# (Unity)</li>
+            <li>Ruby (on Rails)</li>
+        </ol>
+
+        <h3>Tools</h3>
+        <table>
+            <tr>
+                <td>Git</td>
+                <td>NPM</td>
+                <td>Ubuntu</td>
+                <td>React.js</td>
+            </tr>
+            <tr>
+                <td>Trello</td>
+                <td>DNS</td>
+                <td>Chrome Dev Tools</td>
+                <td>CentOS</td>
+                <td>React Native</td>
+            </tr>
+            <tr>
+                <td>cPanel</td>
+                <td>GitHub</td>
+                <td>JetBrains&rsquo; IDEs</td>
+                <td>LAMP stack</td>
+                <td>Node.js</td>
+            </tr>
+            <tr>
+                <td>Bind9</td>
+                <td>Svelte(Kit)</td>
+                <td>Unity</td>
+                <td>Docker</td>
+                <td>MySql</td>
+            </tr>
+        </table>
+
+        <h3>Concepts &amp; General</h3>
+        <table>
+            <tr>
+                <td>Object Oriented Programming</td>
+                <td>Programming Language Design</td>
+                <td>Relational Databases</td>
+            </tr>
+            <tr>
+                <td>Complex Server-Client Models</td>
+                <td>Dynamic Programming</td>
+                <td>Problem Solving &amp; Abstractification</td>
+            </tr>
+        </table>
+    </section>
+
+    <section>
+        <h2>Experience</h2>
+
+        <h3>World Cube Association Software Team</h3>
+        <p>
+            I am on the World Cube Association Software Team (WST).
+            The WST maintains the WCA website, tools, infrastructure
+            and other software that is needed to run the WCA. I help
+            maintain and improve the website of the WCA. At the moment
+            I am helping migrate to React from Ruby on Rails.
+        </p>
+
+        <h3>Work Experience at JumpTech</h3>
+        <p>
+            I spent a week working with the silicon-fen startup JumpTech.
+            During the week, I created some documentation and examples
+            for their API using Postman. I then created a GraphQL
+            client in TypeScript which was used by pen-testers when
+            testing that API. I then did some pre-pentest pen testing
+            of their site, before picking up their TypeScript general
+            utilities project. I went into their office for 4 days and
+            worked remotely the last day. I attended daily standup,
+            organised and went to meetings, and did pair programming
+            with the senior devs and worked independently throughout
+            the week.
+        </p>
+
+        <h3>InvestIn Internship</h3>
+        <p>
+            This 5-day computer science internship covered full-stack
+            development, cybersecurity, building AI, big data and more.
+            I learnt how to pen-test a website; program front-end
+            applications in JavaScript, CSS and HTML, develop a KNN
+            learning algorithm using excel, and all alongside professionals
+            in each aspect of the week.
+        </p>
+    </section>
+
+    <section>
+        <h2>Education</h2>
+
+        <h3>A Levels</h3>
+        <p>
+            <b>King’s Ely, Ely, Cambridgeshire</b>
+            I am currently studying my A levels at King’s Ely.
+            I am taking Maths, Further Maths, Computer Science
+            and Physics. My predicted grades are 4 A*s.
+        </p>
+        <h3>GCSEs</h3>
+        <p>
+            <b>King’s Ely, Ely, Cambridgeshire</b>
+            Computer Science, Further Maths, Design Engineering,
+            Maths, Physics, Chemistry, Biology, and Religious Studies: 9
+            Geography, English Literature and English Language: 8
+        </p>
+
+        <h3>Awards</h3>
+        <ul>
+            <li>King’s Scholarship from King’s Ely (based on GCSE scores)</li>
+            <li>KE (King’s Ely) 6th Form Academic Scholarship</li>
+            <li>KE 6th Form STEM Scholarship</li>
+            <li>2023 TCS Oxford Computing Challenge 44 points, Distinction, Best in School</li>
+            <li>2022 TCS Oxford Computing Challenge 57 points, Distinction, Best in School</li>
+            <li>2022 KE Pye Foundation Prize for Contributions to Computing</li>
+            <li>2022 KE Computer Science Prize</li>
+            <li>2021 Bebras Challenge 170 points, distinction, gold, best in school</li>
+            <li>2021 TCS Oxford Computing Challenge 48 points, distinction</li>
+            <li>Semifinals (Gigafinal) of the Delancey National Chess tournament three times</li>
+            <li>Best Design Award at Robocon 2019</li>
+            <li>Silver in 2021 UK Maths Challenge</li>
+            <li>KE Academic Prize 2021</li>
+            <li>2021 KE Pye Foundation Prize for Contributions to Computing</li>
+            <li>2020 Senior Maths Challenge Bronze Award - Best in Year</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Personal Projects</h2>
+        Most of my projects can be found on my
+        <a href="https://github.com/revers3ntropy">GitHub</a>.
+
+        <h3>Halcyon.Land</h3>
+        <p>
+            <i>SvelteKit, TypeScript, MySQL, Playwright, Apache</i>
+            <a href="https://halcyon.land" target="_blank">Halcyon.Land</a>
+            is a journaling and life logging site I created with SvelteKit.
+        </p>
+
+        <h3>Oxynium</h3>
+        <p>
+            <i>Rust</i>
+            Oxynium is an AOT compiled, statically typed, multi-paradigm,
+            general-purpose programming language.
+            It has a powerful static type system with generic types, classes and primitives.
+            Classes support static or instance methods, operator overloading and more.
+            It has an intuitive syntax and compiles to native machine code, making it
+            blazingly fast - or write inline assembly using the macro system.
+            It has a basic standard library and import system.
+        </p>
+
+        <h3>Entropy Script</h3>
+        <p>
+            <i>TypeScript</i>
+            Entropy Script is the scripting language for Entropy Engine.
+            It uses a syntax similar to TypeScript and Go, and has functions,
+            variables, classes, objects, error handling,
+            tracebacks, runtime type checking and more.
+            It is written entirely from scratch in TypeScript and Javascript:
+            it takes in a string, tokensises, parses and interprets it to run
+            the code.
+            I have also made a translator for it which compiles Entropy Script
+            into JavaScript and Python.
+            <a href="https://entropygames.io/entropy-script/run">Try it out in the web</a> |
+            <a href="https://github.com/revers3ntropy/entropy-script">See the Source Code</a> |
+            <a href="https://entropygames.io/entropy-script/">See the Docs</a> |
+            <a href="https://www.npmjs.com/package/entropy-script">Find it on NPM</a>
+        </p>
+
+        <h3>Sorting Algorithm Visualiser</h3>
+        <p>
+            <i>React.js, JS, HTML, CSS</i>
+            Watch sorting algorithms work with this aesthetically
+            pleasing sorting algorithm visualiser
+            <a href="https://revers3ntropy.com/ess/sorting/build">here</a>.
+            This was my first React project and taught me how to use the framework.
+        </p>
+
+        <h3>Physics Simulator</h3>
+        <p>
+            <i>Javascript, HTMl, CSS</i>
+            In this, you can run many different simulations using a
+            ball collision physics engine. This project was great in
+            combining physics, maths and computer science for me.
+            You can play around with it
+            <a href="http://revers3ntropy.com/ess/particle-physics-engine">here</a>.
+        </p>
+    </section>
+</main>
+
+<style>
+
+    * {
+        color: white;
+    }
+
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+        background: #292a2d;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: fit-content;
+    }
+
+    main {
+        max-width: 800px;
+        padding: 2rem;
+    }
+
+    h1 {
+        font-size: 3rem;
+        font-weight: 300;
+        color: #fff;
+        text-align: center;
+    }
+
+    a {
+        color: #6dffd2;
+    }
+</style>
